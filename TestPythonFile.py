3938c7bedc66fa537c3df31b544ce44ff905629e
amarjot0550 <amarjotsandhu2004@gmail.com>
2023-07-05 15:04:32 -0700

    set_temp function

--- TestPythonFile.py
+++ TestPythonFile.py
@@ -1,4 +1,3 @@
-
 import openai
 import os
 
@@ -6,13 +5,22 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
+temp = None
+
+def set_temp(inItTemp):
+    global temp
+    temp = inItTemp
+
+set_temp(1)
+
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
+        temperature=temp, # this is the degree of randomness of the model's output
     )
+    print("\nTemprature: " + str(temp) + "\n")
     return response.choices[0].message["content"]
 
 # def get_location():
